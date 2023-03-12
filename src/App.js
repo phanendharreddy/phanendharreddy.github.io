71d7780e7cf55164cad8c828a792e3d3c01f82ea
phanendharreddy <phanendharreddykusuma@gmail.com>
2023-03-13 00:37:45 +0530

    added heading in the main

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo}  alt="logo" />        
+        <h1>THIS IS TEST</h1>
       </header>
     </div>
   );

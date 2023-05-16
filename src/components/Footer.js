10d2ebcf116b2785aa1b38e9cd89849d2dae1649
phanendharreddy <phanendharreddykusuma@gmail.com>
2023-05-16 22:41:22 +0530

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,8 +19,10 @@ const Footer = () => {
             </div>
             <div className="right">
             <h4>Personal Note</h4>
-            <p> A quick summary of mine</p>
-            <p> Objectives</p>
+            <p> SUMMARY : Motivated CS graduate skilled in both front-end and back-end development. Strong foundation in data structures and algorithms with proven ability to collaborate and communicate effectively. Committed to staying up-to-date with industry trends.</p>
+            <br></br> \n
+            <p> OBJECTIVES : To secure a challenging role as a software development engineer where I can leverage my passion for software development and work on real-time, high-scalable applications while gaining hands-on experience in the field. As a recent graduate in computer science, I am eager to learn and apply my skills to contribute to the success of a dynamic team.
+            </p>
             <div className="socials"> 
             <a href="https://www.twitter.com/phanendharr_ddy">
             <FaTwitter className="twitter" size={40} style={{color:"#fff", marginRight:"2rem" }}/>
--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -3,24 +3,45 @@ import sde from "../assets/sde.jpeg"
 
 const ProjectCardData = [
     {
+        imgsrc: sde,
+        title: "Cyberbulling Detection using Deep Learning",
+        text:"project description",
+        source:"https://www.github.com/phanendharreddy/"
+        
+    },
+    {
         imgsrc: SarcasamDetection,
-        title: "Extension of Lexicon Algorithm for Sarcasm Detection",
+        title: "Extension of Lexicon Algoritm for Sarcasm Detection",
         text:"project description",
         source:"https://www.github.com/phanendharreddy/sarcasamDetection"
         
     },
     {
         imgsrc: sde,
-        title: "Extension of Lexicon Algoritm for Sarcasm Detection",
+        title: "Weather Web application",
         text:"project description",
-        source:"https://www.github.com/phanendharreddy"
+        source:"https://www.github.com/phanendharreddy/weatherApp"
         
     },
     {
         imgsrc: sde,
-        title: "Extension of Lexicon Algoritm for Sarcasm Detection",
+        title: "Private Browser",
         text:"project description",
-        source:"https://www.github.com/phanendharreddy"
+        source:"https://www.github.com/phanendharreddy/ownBrowser"
+        
+    },
+    {
+        imgsrc: sde,
+        title: "Backend API Service for Authorization and Authentication in Golang",
+        text:"project description",
+        source:"https://www.github.com/phanendharreddy/backendapi"
+        
+    },
+    {
+        imgsrc: sde,
+        title: "My Portfolio Repository",
+        text:"project description",
+        source:"https://www.github.com/phanendharreddy/phanendharreddy.github.io"
         
     },
 ];
